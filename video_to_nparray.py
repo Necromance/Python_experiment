e3df870a73972ab39de217dd97d8d569b3a82734
Necromance <576979604@qq.com>
2017-05-10 14:59:11 +0800

    Update video_to_nparray.py

--- video_to_nparray.py
+++ video_to_nparray.py
@@ -1,7 +1,9 @@
+将视频按帧存储，转换为numpy数组
 import numpy as np
 import cv2
-cap = cv2.VideoCapture('d:\\source.avi')
-count = 0,
+
+cap = cv2.VideoCapture('d:\source.avi')
+count = 0
 big_array = []
 flag,frame = cap.read()
 big_array = frame
@@ -9,7 +11,8 @@ while(flag):
     flag, frame = cap.read()
     if flag == True:
         count += 1
-    print(frame.shape)
-    big_array = np.append(big_array,frame,axis=2)
-    print(count)
-    print(big_array.shape)
+        print(frame.shape)
+        big_array = np.append(big_array,frame,axis=2)
+print(count)
+print(big_array.shape)
+
